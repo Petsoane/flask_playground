b3cf59127f86477c4e313e543a95630b53e79c08
Lebogang PETSOANE <lpetsoan@c4r17s8.wethinkcode.co.za>
2019-11-17 17:09:55 +0200

    Flask is hella fun to user

---
+++ flask_blog/flaskblog/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '371ce8e3adeo7cb21af3ebc9d42bd6b88a0d79890fc531362'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from flaskblog import routes
---
+++ flask_blog/run.py
@@ -0,0 +1,5 @@
+from flaskblog import app
+
+# Run the application if it is main.
+if __name__ == '__main__':
+    app.run(debug=True)
